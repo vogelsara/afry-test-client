7f2ee39df738e08b078f927728ae8914692be8c6
sara_vogel <sara.vogel@libitum.com>
2021-05-18 13:19:23 +0200

    Configure live server

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import CreatePeople from './pages/CreatePeople';
 import Companies from './pages/Companies';
 import PeopleWithoutCompany from './pages/PeopleWithoutCompany';
 
+import axios from 'axios';
+
+if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+  axios.defaults.baseURL = 'http://localhost:5001/afry-test/europe-west1/api';
+} else {
+  axios.defaults.baseURL = 'https://europe-west1-afry-test.cloudfunctions.net/api';
+}
+
 function App() {
   return (
     <div className="App">
