569a99499aa5c91bef9c0b06ddea542c9aacf972
sara_vogel <sara.vogel@libitum.com>
2021-05-19 22:21:01 +0200

    Add trash icon to people sublist under companies

--- src/features/companies/CompanyListElement.js
+++ src/features/companies/CompanyListElement.js
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
 import Collapse from '@material-ui/core/Collapse';
 import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
 import BusinessCenterRoundedIcon from '@material-ui/icons/BusinessCenterRounded';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -43,6 +46,11 @@ export const CompanyList = (props) => {
                 <PersonRoundedIcon />
                 </ListItemIcon>
                 <ListItemText primary={person.name} />
+                <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton>
+                </ListItemSecondaryAction>
             </ListItem>
         );
     })
